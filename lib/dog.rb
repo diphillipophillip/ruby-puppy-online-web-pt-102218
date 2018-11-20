664ceeb0074399c67d9b1a452ce344a54d04e322
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-11-20 01:59:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,23 @@
- 
+
 class Dog 
   
-  @@all = []
-  
   attr_accessor :name 
+  @@all = []
   
   def initialize(name)
     @name = name 
     @@all << self
   end 
   
- def self.all 
-   @@all.each do |i|
-     puts i.name
-   end 
+  def self.all 
+    @@all.each do |i| 
+      puts i.name
+    end
   end 
+    
+    def self.clear_all 
+      @@all.clear 
+    end 
   
-  def self.clear_all
-    @@all.clear 
-
-  end 
-
-
 end 
-
-
 
